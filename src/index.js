6d8f4e80ae9baed2b457624149253bd79b5adcc4
Gaurav Thakur <gthakur581@gmail.com>
2021-01-16 14:17:18 +0530

    Linked the service worker to fcm.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ import { BrowserRouter } from "react-router-dom";
 import { MuiThemeProvider } from "@material-ui/core";
 import theme from "./config/theme";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./firebase-messaging-sw.js")
+    .then(function (registration) {
+      console.log("Registration successful, scope is:", registration.scope);
+    })
+    .catch(function (err) {
+      console.log("Service worker registration failed, error:", err);
+    });
+}
 ReactDOM.render(
   <BrowserRouter>
     <AuthProvider>
